9b27c75a8613701707577b34a7417294604a218a
keeprok <140785214+keeprok@users.noreply.github.com>
2025-09-30 18:47:51 +0900

    feat : chip 컴포넌트 생성

---
+++ src/stories/Chip.tsx
@@ -0,0 +1,24 @@
+import { cn } from '../utils/cn';
+interface ChipProps {
+  children: string;
+  size?: 'sm' | 'lg';
+  isActive: boolean;
+  onClick: () => void;
+}
+export const Chip = ({ children, size = 'lg', isActive, onClick }: ChipProps) => {
+  return (
+    <div
+      role="button"
+      onClick={() => {
+        if (!isActive) onClick();
+      }}
+      className={cn(
+        'select-none rounded-full font-medium transition-colors min-w-12  items-center justify-center h-9 inline-flex',
+        size === 'lg' ? ' px-4 text-sm' : ' px-3 text-xs',
+        isActive ? 'bg-gray-900 text-white pointer-events-none' : 'bg-gray-50 text-black cursor-pointer'
+      )}
+    >
+      {children}
+    </div>
+  );
+};
