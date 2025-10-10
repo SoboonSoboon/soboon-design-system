3339456599475335a49093c737ec46a7e1c8fb2e
kim dong hyeon <kdh990315@naver.com>
2025-10-10 13:26:49 +0900

    remove: 불필요한 파일 삭제

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  transform: { '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
-};
